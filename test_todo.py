0b260422c8bb09b96c1c001f282ec4964ddff9ce
Baiju Muthukadan <baiju.m.mail@gmail.com>
2013-08-15 12:04:56 +0530

    Stage 2

--- test_todo.py
+++ test_todo.py
@@ -7,3 +7,12 @@ def test_simple():
     time.sleep(5)
     assert driver.title == 'TODO List'
     driver.quit()
+
+def test_add_new_item():
+    driver = webdriver.Firefox()
+    driver.get('http://localhost:8080/')
+    time.sleep(2)
+    element = driver.find_element_by_link_text('New')
+    element.click()
+    time.sleep(2)
+    driver.quit()
